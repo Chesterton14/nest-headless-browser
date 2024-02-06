4f3061513697e92463b65550d0502fdb699ec7d6
Zoo Xu <chesterton14@163.com>
2024-02-06 11:25:54 +0800

    feat: add puppeteer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import puppeteer, { Browser } from 'puppeteer';
+
+let browser: Browser;
+
+async function getBrowser() {
+  if (!browser) {
+    browser = await puppeteer.launch({
+      headless: true,
+    });
+  }
+  return browser;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('ws/endpoint')
+  async getWsEndpoint() {
+    const browser = await getBrowser();
+    const browserWSEndpoint = browser.wsEndpoint();
+    await browser.disconnect();
+    return browserWSEndpoint;
   }
 }
